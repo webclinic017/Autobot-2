e2a19db7ca95130d143f6e076ea2230d1ebb60a9
aseldar <aseldar@inbox.ru>
2020-12-13 23:02:28 +0300

    raly comit

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'index'
+    title = 'Trading Bot!'
+    return render_template('index.html', title = title)
 
 @app.route('/buy')
 def buy():
