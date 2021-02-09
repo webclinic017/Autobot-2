290a13ae9f0cac45fe177281ecd67268c1c81a6b
aseldar <aseldar@inbox.ru>
2021-02-09 14:10:14 +0300

    comit 09.09

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, url_for
+from binance.client import Client
+from datetime import datetime, timedelta
+import config
+from binance.enums import *
+
+client = Client(config.API_KEY, config.API_SECRET)
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     title = 'Trading Bot!'
-    return render_template('index.html', title = title)
+    info = client.get_account()
+    balance = info['balances']
+    list_of_balance = [[i['asset'], i['free'], i['locked']] for i in info['balances'] if float(i['free'])>0.]
+    return render_template('index.html', title = title, my_balance=list_of_balance)
 
 @app.route('/buy')
 def buy():
@@ -17,6 +27,26 @@ def sell():
 @app.route('/setting')
 def setting():
     return 'setting'
+
+
+@app.route('/history')
+def history():
+    candels = client.get_historical_klines("BTCUSDT", Client.KLINE_INTERVAL_1HOUR, "19 Dec, 2020", '{:%d %B, %Y}'.format(datetime.today() + timedelta(days=1))) # , "1 Jan, 2017", "6 Dec, 2020"
+
+    list_canel = []
+    for i in candels:
+        points = {
+            'time': i[0]/1000,
+            'open': i[1],
+            'high': i[2],
+            'low': i[3],
+            'close': i[4]
+        }
+        list_canel.append(points)
+
+
+    return jsonify(list_canel)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- get_data.py
+++ get_data.py
@@ -1,10 +1,20 @@
 from binance.client import Client
 import config
+from datetime import datetime, timedelta
 import numpy as np
 import pandas as pd
 import csv
 
 client = Client(config.API_KEY, config.API_SECRET)
+
+info = client.get_account()
+# for i in info['balances']:
+#     # print(i['loced'])
+#     if float(i['free'])>0.:
+#         print(i)
+
+# list_of_balance = [[i['asset'], i['free'], i['locked']] for i in info['balances'] if float(i['free'])>0.]
+# print(list_of_balance)
 
 # prices = client.get_all_tickers()
 
@@ -23,15 +33,54 @@ client = Client(config.API_KEY, config.API_SECRET)
 # print(df.shape)
 # print(len(candles))
 
-# candels = client.get_historical_klines("BTCUSDT", Client.KLINE_INTERVAL_5MINUTE, "1 Jan, 2017", "12 Dec, 2020") # , "1 Jan, 2017", "6 Dec, 2020"
 
 
-# csvfile = open('5_min.csv', 'w', newline='')
+
+# candels = client.get_historical_klines("BTCUSDT", Client.KLINE_INTERVAL_30MINUTE, "19 Dec, 2020", '{:%d %B, %Y}'.format(datetime.today() + timedelta(days=1))) # , "1 Jan, 2017", "6 Dec, 2020"
+
+# list_canel = []
+# for i in candels:
+#     points = {
+#         'time': i[0],
+#         'open': i[1],
+#         'high': i[2],
+#         'low': i[3],
+#         'close': i[4]
+#     }
+#     list_canel.append(points)
+
+# print(list_canel[-1])
+
+
+# Get dayly data
+# candels = client.get_historical_klines("BTCUSDT", Client.KLINE_INTERVAL_1DAY, "1 Jan, 2020", '{:%d %B, %Y}'.format(datetime.today() + timedelta(days=1))) # , "1 Jan, 2017", "6 Dec, 2020"
+
+# csvfile = open('data/dayly.csv', 'w', newline='')
 # candel_writer = csv.writer(csvfile, delimiter=',')
 
 # for candel in candels:
-#     #print(candel)
+#     candel[0] = datetime.utcfromtimestamp(int(candel[0]/1000)).strftime('%Y-%m-%d %H:%M:%S') 
 #     candel_writer.writerow(candel)
 
 # print(len(candels))
 # csvfile.close()
+
+# print('{:%d %B, %Y}'.format(datetime.today() + timedelta(days=1)))
+
+# Get 15 min data
+candels = client.get_historical_klines("BTCUSDT", Client.KLINE_INTERVAL_1HOUR, "1 Oct, 2020", '{:%d %B, %Y}'.format(datetime.today() + timedelta(days=1))) # , "1 Jan, 2017", "6 Dec, 2020"
+
+csvfile = open('data/1hour_backtest.csv', 'w', newline='')
+candel_writer = csv.writer(csvfile, delimiter=',')
+
+for candel in candels:
+    candel[0] = datetime.utcfromtimestamp(int(candel[0]/1000)).strftime('%Y-%m-%d %H:%M:%S') 
+    candel_writer.writerow(candel)
+
+print(len(candels))
+csvfile.close()
+
+
+
+
+
